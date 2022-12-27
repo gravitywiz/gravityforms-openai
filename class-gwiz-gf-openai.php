d83c2adad7d60cb01ae42370ef1164e86611149b
Clay Griffiths <clay@claygriffiths.com>
2022-12-27 14:46:26 -0600

    Added new `gf_openai_request_body` PHP filter for customizing requests that go to OpenAI.
    Example usage:
    
    ```php
    // Add separator to end of prompts for fine-tuned model.
    add_filter( 'gf_openai_request_body', function ( $body, $endpoint, $feed ) {
    	if ( $endpoint !== 'completions' || rgars( $feed, 'meta/completions_model' ) !== 'ada:ft-personal-2022-12-27-20-25-37' ) {
    		return $body;
    	}
    
    	$body['prompt'] = $body['prompt'] . "\n\n###\n\n";
    
    	return $body;
    }, 10, 3 );
    ```

--- class-gwiz-gf-openai.php
+++ class-gwiz-gf-openai.php
@@ -1456,6 +1456,8 @@ class GWiz_GF_OpenAI extends GFFeedAddOn {
 				break;
 		}
 
+		$body = apply_filters( 'gf_openai_request_body', $body, $endpoint, $feed );
+
 		// Cache successful responses.
 		$response = wp_remote_post($url, array_merge( array(
 			'body' => json_encode( $body ),
